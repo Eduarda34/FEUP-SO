4228f0dc2f4933ed6c56b1393d1fe532d72ff86e
charchalis <charchalis97@gmail.com>
2022-11-27 17:04:35 +0000

    something somethging modern problems

--- samples/run.sh
+++ samples/run.sh
@@ -1,2 +1,2 @@
 gcc samples.c -o samples
-./samples quote.txt 6 5
+./samples quote.txt 15 9
--- samples/samples.c
+++ samples/samples.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 
 //returns EXIT_FAILURE if snippet of text has a "\n"
@@ -13,6 +15,10 @@ int checkParagraph(char* str){
 
 
 int main(int argc, char* argv[]){
+
+  time_t tt;
+  srand((int)time(&tt) % getpid());
+
   FILE * fp;
 
   fp = fopen(argv[1], "r");
@@ -31,12 +37,28 @@ int main(int argc, char* argv[]){
   printf("file length: %d\n\n", len);
 
   char buffer[atoi(argv[3])];
+  
+  //printf("atoi(argv[3])= %d\n", atoi(argv[3]));
+
+  //printf("buffer.size = %d\n", strlen(buffer));
+
+  //char buffer = (char)malloc((atoi(argv[3])+1) * sizeof(char));
+
+  fclose(fp);
 
   for(int i = 0; i < atoi(argv[2]); i++){
     
+    fp = fopen(argv[1], "r");
+
     //printf("rand: %d\n", rand() % len);
+
+    
     
     int offset = rand() % len;
+    while(1){
+      if((len - offset) < atoi(argv[3])) offset = rand() % len;
+      else break;
+    }
 
     fseek( fp, offset, SEEK_SET );
 
@@ -45,17 +67,34 @@ int main(int argc, char* argv[]){
     while(checkParagraph(buffer) == EXIT_FAILURE){
 
       offset = rand() % len;
+      while(1){
+	if((len - offset) < atoi(argv[3])) offset = rand() % len;
+	else break;
+      }
       fseek( fp, offset, SEEK_SET );
       fread(buffer, atoi(argv[3]), 1, fp);
     
     }
-      
-    printf(">%s<\n", buffer);
     
+    //printf("buffer.size = %d\n", strlen(buffer));
+    
+    char result[atoi(argv[3])];
+
+    printf(">");
+
+    for(int i = 0; i < atoi(argv[3]); i++){
+      printf("%c",buffer[i]);
+    }
+    printf("<\n");
+    
+    //printf(">%s<\n", result);
+    
+    fclose(fp);
+
   }
 
 
-  fclose(fp);
+  //fclose(fp);
 
   exit(EXIT_SUCCESS);  
 }
