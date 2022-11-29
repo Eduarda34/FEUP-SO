17774c87f9eee031967e302006eb17bbf3aecba7
charchalis <charchalis97@gmail.com>
2022-11-29 21:01:58 +0000

    q2 bug fix

--- txt2epub/txt2epub.c
+++ txt2epub/txt2epub.c
@@ -32,38 +32,58 @@ char* substr(const char *src, int m, int n)
 
 int main(int argc, char* argv[]){
 
+  printf("%d\n", argc);
+
+  char* books[argc - 1];
+  int zipCommandLength = strlen("zip ebooks.zip ");
+
   for(int i = 1; i < argc; i++){
 
-    char command[500];
-
-    snprintf(command, sizeof(command), "pandoc %s -o %s.epub >/dev/null 2>&1  &&  rm %s", argv[i], substr(argv[i], 0, strlen(argv[i]) - 4), argv[i]);
-
-    //printf("%s\n", command);
+    char* book = strcat(substr(argv[i], 0, strlen(argv[i]) - 4), ".epub");
+    books[i - 1] = book;
+    zipCommandLength += strlen(book) + 1;
 
     if(fork() == 0){
+
+      char command[500];
+
+      snprintf(command, sizeof(command), "pandoc %s -o %s >/dev/null 2>&1  &&  rm %s", argv[i], book, argv[i]);
+
       
       printf("[pid%d] converting %s...\n", getpid(), argv[i]);
 
-      int success = system(command);
-      exit(success);
+      exit(system(command));
     }
   
   }
 
-  /* Wait for children */
-    int corpse;
-    int status; // if stattus == 0 then great success 
-    while ((corpse = wait(&status)) > 0){
-	
-	if(status == 1){
-	  printf("FAILLLLLLEDDDDDD");
-	  return EXIT_FAILURE;
-	}
 
+
+  /* Wait for children */
+  
+  int corpse;
+  int status; // if stattus == 0 then great success 
+  while ((corpse = wait(&status)) > 0){
+	
+    if(status == EXIT_FAILURE){
+      printf("FAILLLLLLEDDDDDD");
+      return EXIT_FAILURE;
     }
+  }
+
+
+
+  /*zip stuff*/
+
+  char zipCommand[zipCommandLength];
+  strcat(zipCommand, "zip ebooks.zip ");
+
+  for(int i = 0; i < argc - 1;i++){
+    strcat(zipCommand, strcat(books[i], " "));
+  }
 
   printf("\nzipping files...\n");
-  system("zip ebooks.zip *.epub");
+  system(zipCommand);
 
   return EXIT_SUCCESS;
 
